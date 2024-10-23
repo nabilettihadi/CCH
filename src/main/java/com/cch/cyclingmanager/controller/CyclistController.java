390451a9120cef17bb808a9cd35718c43ec1d25b
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-10-23 14:57:39 +0100

    Ajout des Controleurs de Cyclist et Team

--- src/main/java/com/cch/cyclingmanager/controller/CyclistController.java
+++ src/main/java/com/cch/cyclingmanager/controller/CyclistController.java
@@ -1,5 +1,52 @@
 package com.cch.cyclingmanager.controller;
 
+import com.cch.cyclingmanager.dto.CyclistDto;
+import com.cch.cyclingmanager.service.CyclistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/cyclists")
 public class CyclistController {
-    
+
+    @Autowired
+    private CyclistService cyclistService;
+
+    @GetMapping
+    public ResponseEntity<List<CyclistDto>> getAllCyclists() {
+        List<CyclistDto> cyclists = cyclistService.findAll();
+        return new ResponseEntity<>(cyclists, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CyclistDto> getCyclistById(@PathVariable Long id) {
+        return cyclistService.findById(id)
+                .map(cyclist -> new ResponseEntity<>(cyclist, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<CyclistDto> createCyclist(@RequestBody CyclistDto cyclistDto) {
+        CyclistDto createdCyclist = cyclistService.save(cyclistDto);
+        return new ResponseEntity<>(createdCyclist, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CyclistDto> updateCyclist(@PathVariable Long id, @RequestBody CyclistDto cyclistDto) {
+        if (!id.equals(cyclistDto.getId())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        CyclistDto updatedCyclist = cyclistService.update(cyclistDto);
+        return new ResponseEntity<>(updatedCyclist, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCyclist(@PathVariable Long id) {
+        cyclistService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/com/cch/cyclingmanager/controller/TeamController.java
+++ src/main/java/com/cch/cyclingmanager/controller/TeamController.java
@@ -1,5 +1,52 @@
 package com.cch.cyclingmanager.controller;
 
+import com.cch.cyclingmanager.dto.TeamDto;
+import com.cch.cyclingmanager.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/teams")
 public class TeamController {
-    
+
+    @Autowired
+    private TeamService teamService;
+
+    @GetMapping
+    public ResponseEntity<List<TeamDto>> getAllTeams() {
+        List<TeamDto> teams = teamService.findAll();
+        return new ResponseEntity<>(teams, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TeamDto> getTeamById(@PathVariable Long id) {
+        return teamService.findById(id)
+                .map(team -> new ResponseEntity<>(team, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<TeamDto> createTeam(@RequestBody TeamDto teamDto) {
+        TeamDto createdTeam = teamService.save(teamDto);
+        return new ResponseEntity<>(createdTeam, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TeamDto> updateTeam(@PathVariable Long id, @RequestBody TeamDto teamDto) {
+        if (!id.equals(teamDto.getId())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        TeamDto updatedTeam = teamService.update(teamDto);
+        return new ResponseEntity<>(updatedTeam, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTeam(@PathVariable Long id) {
+        teamService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
