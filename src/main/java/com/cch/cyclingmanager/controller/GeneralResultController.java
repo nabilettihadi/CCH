e41494b1912ad78df56c9bad7d2aa767b958645b
Nabil Ettihadi <nabilettihadi.ma@gmail.com>
2024-10-24 11:40:09 +0100

    Ajout de Controleur de General Result

--- src/main/java/com/cch/cyclingmanager/controller/GeneralResultController.java
+++ src/main/java/com/cch/cyclingmanager/controller/GeneralResultController.java
@@ -1,5 +1,61 @@
 package com.cch.cyclingmanager.controller;
 
+import com.cch.cyclingmanager.dto.GeneralResultDto;
+import com.cch.cyclingmanager.entity.embeddable.GeneralResultId;
+import com.cch.cyclingmanager.service.GeneralResultService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/general-results")
 public class GeneralResultController {
-    
+
+    @Autowired
+    private GeneralResultService generalResultService;
+
+    @GetMapping
+    public ResponseEntity<List<GeneralResultDto>> getAllGeneralResults() {
+        List<GeneralResultDto> generalResults = generalResultService.findAll();
+        return new ResponseEntity<>(generalResults, HttpStatus.OK);
+    }
+
+    @GetMapping("/{competitionId}/{cyclistId}")
+    public ResponseEntity<GeneralResultDto> getGeneralResultById(
+            @PathVariable Long competitionId,
+            @PathVariable Long cyclistId) {
+        return generalResultService.findById(new GeneralResultId(competitionId, cyclistId))
+                .map(generalResult -> new ResponseEntity<>(generalResult, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<GeneralResultDto> createGeneralResult(@RequestBody GeneralResultDto generalResultDto) {
+        GeneralResultDto createdGeneralResult = generalResultService.save(generalResultDto);
+        return new ResponseEntity<>(createdGeneralResult, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{competitionId}/{cyclistId}")
+    public ResponseEntity<GeneralResultDto> updateGeneralResult(
+            @PathVariable Long competitionId,
+            @PathVariable Long cyclistId,
+            @RequestBody GeneralResultDto generalResultDto) {
+        if (!competitionId.equals(generalResultDto.getCompetitionId()) ||
+                !cyclistId.equals(generalResultDto.getCyclistId())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        GeneralResultDto updatedGeneralResult = generalResultService.save(generalResultDto);
+        return new ResponseEntity<>(updatedGeneralResult, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{competitionId}/{cyclistId}")
+    public ResponseEntity<Void> deleteGeneralResult(
+            @PathVariable Long competitionId,
+            @PathVariable Long cyclistId) {
+        generalResultService.deleteById(new GeneralResultId(competitionId, cyclistId));
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
